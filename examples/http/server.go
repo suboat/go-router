eb70bc432ecf2665d95d5a9bc06cb4600236327a
wd <wd@suboat.com>
2016-04-18 18:44:46 +0800

    feat: add gin plug-in

--- examples/http/server.go
+++ examples/http/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	. "github.com/suboat/go-router/routers/gin"
 	. "github.com/suboat/go-router/routers/mux"
 	"log"
 	"net/http"
@@ -14,20 +15,36 @@ func logS(v ...interface{}) {
 }
 
 func startServer() {
-	switch 0 {
+	switch 1 {
 	case 0:
 		r := NewMuxRouter().PathPrefix("/v1")
 		r.HandleFunc("/",
 			func(w http.ResponseWriter, r *http.Request) {
+				logS(fmt.Sprintf("%#v", r))
 			},
 		).Methods("GET")
 		r.HandleFunc("/id/{id}",
 			func(w http.ResponseWriter, r *http.Request) {
+				logS(fmt.Sprintf("%#v", r))
 			},
 		).Methods("GET")
 		logS("start...")
 		exit <- r.Error()
 		exit <- r.ListenAndServe(host_server)
 	case 1:
+		r := NewGinRouter().PathPrefix("/v1")
+		r.HandleFunc("/",
+			func(w http.ResponseWriter, r *http.Request) {
+				logS(fmt.Sprintf("%#v", r))
+			},
+		).Methods("GET")
+		r.HandleFunc("/id/:id",
+			func(w http.ResponseWriter, r *http.Request) {
+				logS(fmt.Sprintf("%#v", r))
+			},
+		).Methods("GET")
+		logS("start...")
+		exit <- r.Error()
+		exit <- r.ListenAndServe(host_server)
 	}
 }
--- routers/gin/router.go
+++ routers/gin/router.go
@@ -7,32 +7,50 @@ import (
 )
 
 type GinRouter struct {
+	Engine *gin.Engine
 	Router *gin.RouterGroup
+	route  *ginRoute
 	err    error
 }
 
-func newGinRouter(r *gin.RouterGroup) *GinRouter {
-	return &GinRouter{Router: r}
+func newGinRouter(e *gin.Engine, r *gin.RouterGroup) *GinRouter {
+	return &GinRouter{Engine: e, Router: r}
 }
 
-func NewGinRouter(r *gin.Engine) *GinRouter {
-	return newGinRouter(r.RouterGroup)
+func NewGinRouterWithEngine(r *gin.Engine) *GinRouter {
+	return newGinRouter(r, &r.RouterGroup)
 }
 
-func (r *GinRouter) newRoute() *mux.Route {
-	r.route = r.Router.NewRoute()
+func NewGinRouter() *GinRouter {
+	return NewGinRouterWithEngine(gin.New())
+}
+
+type ginRoute struct {
+	Router  *gin.RouterGroup
+	Path    string
+	Handler gin.HandlerFunc
+}
+
+func (s *ginRoute) Handle(method string) {
+	if len(s.Path) != 0 {
+		s.Router.Handle(method, s.Path, s.Handler)
+	}
+}
+
+func (r *GinRouter) newRoute(path string, handler gin.HandlerFunc) *ginRoute {
+	r.route = &ginRoute{Router: r.Router, Path: path, Handler: handler}
 	return r.route
 }
 
-func (r *GinRouter) getRoute() *mux.Route {
+func (r *GinRouter) getRoute() *ginRoute {
 	if r.route == nil {
-		return r.newRoute()
+		return &ginRoute{}
 	}
 	return r.route
 }
 
 func (r *GinRouter) ListenAndServe(addr string) error {
-	r.err = http.ListenAndServe(addr, r.Router)
+	r.err = r.Engine.Run(addr)
 	return r.err
 }
 
@@ -41,20 +59,26 @@ func (r *GinRouter) Error() error {
 }
 
 func (r *GinRouter) Handle(path string, handler http.Handler) HTTPRoute {
-	r.newRoute().Path(path).Handler(handler)
+	r.newRoute(path, gin.WrapH(handler))
 	return r
 }
 
 func (r *GinRouter) HandleFunc(path string, handler func(http.ResponseWriter, *http.Request)) HTTPRoute {
-	r.newRoute().Path(path).HandlerFunc(handler)
+	r.newRoute(path, gin.WrapF(handler))
 	return r
 }
 
+func (r *GinRouter) HandleBind(path string, handler HandleBind) HTTPRoute {
+	return r.Handle(path, handler)
+}
+
 func (r *GinRouter) Methods(methods ...string) HTTPRoute {
-	r.getRoute().Methods(methods...)
+	for _, method := range methods {
+		r.getRoute().Handle(method)
+	}
 	return r
 }
 
 func (r *GinRouter) PathPrefix(tpl string) HTTPRoute {
-	return newGinRouter(r.newRoute().PathPrefix(tpl).Subrouter())
+	return newGinRouter(r.Engine, r.Router.Group(tpl))
 }
